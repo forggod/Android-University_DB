5c1cc86e2eade2006fbc36123f2752c270141947
Фёдор Ситников <mr.prizrek@mail.ru>
2023-05-25 23:09:16 +0300

    Исправленно отображение даты

--- app/src/main/java/com/example/second34_2/ui/StudentFragment.kt
+++ app/src/main/java/com/example/second34_2/ui/StudentFragment.kt
@@ -3,6 +3,7 @@ package com.example.second34_2.ui
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -54,24 +55,15 @@ class StudentFragment : Fragment() {
             binding.editTextTextPersonNameName.setText(student!!.middleName)
             binding.editTextTextPersonNameLastName.setText(student!!.lastName)
             binding.editTextPhone.setText(student!!.phone)
-            val date = GregorianCalendar.getInstance()
-            date.time.time = student!!.birthDate!!
-
-            binding.calendarID.init(
-                date.get(Calendar.YEAR), date.get(Calendar.MONTH),
-                date.get(Calendar.DAY_OF_MONTH), null
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = student!!.birthDate!!
+            binding.calendarID.updateDate(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
             )
         }
         viewModel = ViewModelProvider(this).get(StudentViewModel::class.java)
-
-        binding.calendarID.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
-            selectedDate.apply {
-                set(GregorianCalendar.YEAR, year)
-                set(GregorianCalendar.MONTH, monthOfYear)
-                set(GregorianCalendar.DAY_OF_MONTH, dayOfMonth)
-            }
-            val date = Date(selectedDate.time.time)
-        }
     }
 
     private val backPressedCallback = object : OnBackPressedCallback(true) {
@@ -115,23 +107,30 @@ class StudentFragment : Fragment() {
             }
 
             if (p) {
-                val selectedDate = GregorianCalendar().apply {
-                    set(GregorianCalendar.YEAR, binding.calendarID.year)
-                    set(GregorianCalendar.YEAR, binding.calendarID.month)
-                    set(GregorianCalendar.YEAR, binding.calendarID.dayOfMonth)
-                }
+                val calendar = Calendar.getInstance()
+                calendar.set(
+                    binding.calendarID.year,
+                    binding.calendarID.month,
+                    binding.calendarID.dayOfMonth
+                )
                 val firstName = binding.editTextTextPersonNameFirstName.text.toString()
                 val lastName = binding.editTextTextPersonNameLastName.text.toString()
                 val middleName = binding.editTextTextPersonNameName.text.toString()
                 val phone = binding.editTextPhone.text.toString()
-                val date = selectedDate.time.time
+                val date = calendar.timeInMillis
+                Log.d(
+                    "STD", "DATA id: ${student?.id} groupId: ${student?.groupId} " +
+                            "firstName: $firstName middleName: $middleName " +
+                            "lastName: $lastName phone: $phone " +
+                            "date: $date"
+                )
                 if (student == null) {
                     CoroutineScope(Dispatchers.Main).launch {
                         FacultyRepository.get()
                             .newStudent(groupID!!, firstName, lastName, middleName, phone, date)
                     }
                 }
-                if (student != null){
+                if (student != null) {
                     val studentNew = Student(
                         id = student!!.id,
                         groupId = student!!.groupId,
